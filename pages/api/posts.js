35d4f0e4cee92f14caa0c1b45eb58cfc6e242b13
Zamur Vedovatto <vedovatto@gmail.com>
2020-08-10 20:04:52 -0300

    ajustes

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,19 +1,18 @@
 import posts from './posts.json'
 
 export default (req, res) => {
-  let data = [];
-  console.log(req.params);
-
-  if (req.params.id) {
-    let id = req.params.id;
-    posts.map(post => {
-      if (post.id === id) {
-        console.log(post)
-        data = post;
-      }
-    })
-  } else {
-    data = posts;
-  }
-  res.status(200).json(data)
+  // let data = [];
+  // if (req.params.id) {
+  //   let id = req.params.id;
+  //   posts.map(post => {
+  //     if (post.id === id) {
+  //       console.log(post)
+  //       data = post;
+  //     }
+  //   })
+  // } else {
+  //   data = posts;
+  // }
+  // res.status(200).json(data);
+  res.status(200).json(posts);
 }
